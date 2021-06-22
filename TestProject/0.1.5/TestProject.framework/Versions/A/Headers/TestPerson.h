b2c909f036d963599a892406cccfda103bca6be6
wanghouyi <wanghouyi@hupu.com>
2021-06-22 16:16:26 +0800

    0.1.5

---
+++ TestProject/0.1.5/TestProject.framework/Versions/A/Headers/TestPerson.h
@@ -0,0 +1,18 @@
+//
+//  TestPerson.h
+//  TestProject
+//
+//  Created by 王厚一 on 2021/6/21.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestPerson : NSObject
+
+- (void)foo;
+
+@end
+
+NS_ASSUME_NONNULL_END
